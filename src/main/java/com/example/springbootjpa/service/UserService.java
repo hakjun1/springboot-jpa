24e5c907738b65836fc840c638d5a33f1509029b
Hakjun <hiatus9014@gmail.com>
2022-11-17 14:50:31 +0900

    addUser @PostMapping 추가

--- src/main/java/com/example/springbootjpa/service/UserService.java
+++ src/main/java/com/example/springbootjpa/service/UserService.java
@@ -1,6 +1,7 @@
 package com.example.springbootjpa.service;
 
 import com.example.springbootjpa.model.User;
+import com.example.springbootjpa.model.dto.UserRequest;
 import com.example.springbootjpa.model.dto.UserResponse;
 import com.example.springbootjpa.repository.UserRepository;
 import org.springframework.stereotype.Service;
@@ -23,5 +24,20 @@ public class UserService {
             User user = optUser.get();
             return new UserResponse(user.getId(), user.getUsername(), "");
         }
+    }
+
+    public UserResponse addUser(UserRequest dto) {
+        User user = dto.toEntity();
+        // dto를 entity로 바꾸는 과정 userrequest에 toentity추가
+        //저장하기 전 username으로 select
+        //있으면 중복되었습니다 메세지로 알려주소, save하지 않음
+        Optional<User> selectedUser = userRepository.findByUsername(dto.getUsername());
+        if (selectedUser.isEmpty()) {
+            User savedUser = userRepository.save(user);
+            return new UserResponse(savedUser.getId(), savedUser.getUsername(), "회원 등록 성공");
+        } else {
+            return new UserResponse(null, dto.getUsername(), "이 user는 이미 존재합니다 다른이름을 사용하세요");
+        }
+
     }
 }
