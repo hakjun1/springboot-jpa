8b6f69bb13217ede1b86a41e4cfedc3db06bdf1a
Hakjun <hiatus9014@gmail.com>
2022-11-17 13:38:12 +0900

    User commit 11.17

---
+++ src/main/java/com/example/springbootjpa/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.example.springbootjpa.controller;
+
+import com.example.springbootjpa.model.dto.UserResponse;
+import com.example.springbootjpa.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponse> getUser(@PathVariable Long id) {
+        return ResponseEntity.ok().body(userService.getUser(id));
+    }
+}
